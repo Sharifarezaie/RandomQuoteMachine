f72a0cd9f79791771429e58d9dba13921ad1efe7
Sharifarezaie <Sharifashaida82@gmail.com>
2024-08-25 21:07:34 +0800

    linter issue

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 const quoteData = [
-  { text: `“The purpose of our lives is to be happy.”`, author: 'Dalai Lama' },
-  { text: `“Life is what happens when you’re busy making other plans.”`, author: 'John Lennon' },
-  { text: `“Get busy living or get busy dying.”`, author: 'Stephen King' },
-  { text: `“You only live once, but if you do it right, once is enough.”`, author: 'Mae West' },
-  { text: `“Many of life’s failures are people who did not realize how close they were to success when they gave up.”`, author: 'Thomas A. Edison' },
+  { text: '“The purpose of our lives is to be happy.”', author: 'Dalai Lama' },
+  { text: '“Life is what happens when you’re busy making other plans.”', author: 'John Lennon' },
+  { text: '“Get busy living or get busy dying.”', author: 'Stephen King' },
+  { text: '“You only live once, but if you do it right, once is enough.”', author: 'Mae West' },
+  { text: '“Many of life’s failures are people who did not realize how close they were to success when they gave up.”', author: 'Thomas A. Edison' },
 ];
 
 const QuoteBox = ({ quote, handleNewQuote }) => (
